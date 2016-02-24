61617c195ed15735ab0b4ac33f49f882e94330fe
ChrisJu96 <christopher.junker@web.de>
2016-02-24 13:01:59 +0100

    Added Background, Added Player Movement (Horizontal)

--- GetSticked/Assets/Scripts/PlayerController.cs
+++ GetSticked/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float speed = 3.0f;
+    private Rigidbody rb;
 
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement* speed);
+    }
 }
